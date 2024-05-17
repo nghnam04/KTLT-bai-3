9e9970532a2f3bc905ef7ae51f74be0542ae5771
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2024-05-17 20:08:52 +0700

    Bài 6 tìm ước chung lớn nhất

---
+++ bt3.6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b){
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int gcd2(int a, int b){
+    
+    //# Khử đệ quy
+    /*
+    Ho va ten: Nguyen Hoang Nam
+    MSSV: 20225213
+    Ma lop: 738920*/
+
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << endl << gcd2(a, b);
+    return 0;
+}
